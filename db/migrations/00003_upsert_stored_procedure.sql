114011a77cecb2e1e368149f3f314b1323795646
Ashish Girish Tutakne <ashishtutaknedev@gmail.com>
2026-02-23 16:27:47 +0000

    Correcting a bug

--- db/migrations/00003_upsert_stored_procedure.sql
+++ db/migrations/00003_upsert_stored_procedure.sql
@@ -12,12 +12,12 @@ BEGIN
     -- Check if the barcode is too long. If this is the case, the subsequent LPAD step 
     -- will strip values from the right, leading to an incorrect (And potentially duplicable)
     -- barcode being uploaded. We never expect the cleaned barcode to be longer than 128 characters.
-    IF char_length(v_clean_barcode) > 130 THEN
+    IF char_length(p_barcode) > 130 THEN
         RAISE EXCEPTION 'Please check barcode %s. Even after removing invalid characters, it is too long (Greater than 48 characters).', p_barcode;
     END IF;
 
     -- Pad the barcode to 130 characters long and make it upper case for consistency.
-    v_clean_barcode := LPAD(UPPER(v_clean_barcode), 130, '0');
+    v_clean_barcode := LPAD(UPPER(p_barcode), 130, '0');
 
     -- Given a clean barcode, we now upsert the product.
     INSERT INTO dirac.product(name, description, barcode)
