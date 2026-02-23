e19c307cbb50b6fe11ced17c1c4809aad62ca070
Ashish Girish Tutakne <ashishtutaknedev@gmail.com>
2026-02-23 14:34:42 +0000

    Correct the normalization as some bar-codes can have special characters

--- db/migrations/00003_upsert_stored_procedure.sql
+++ db/migrations/00003_upsert_stored_procedure.sql
@@ -9,15 +9,6 @@ CREATE OR REPLACE PROCEDURE dirac.ingest_product(
 DECLARE
     v_clean_barcode VARCHAR;
 BEGIN
-    -- Normalize the barcode.
-    -- The normalization is done by removing all non numeric, letter or "^" characters, 
-    -- as these are the only characters we expect to see in a barcode. We then add
-    -- leading zeros to pad it to 50 digits long. This allows us to normalize barcodes
-    -- of different formats, such as EAN-13, UPC-A, etc. The data limit of a code 128
-    -- barcode is 48 charactsr, making 50 sufficient. We also set the barcode to be 
-    -- uppercase to ensure consistency.
-    v_clean_barcode := UPPER(REGEXP_REPLACE(p_barcode, '[^a-zA-Z0-9]', '', 'g'));
-    
     -- Check if the barcode is too long. If this is the case, the subsequent LPAD step 
     -- will strip values from the right, leading to an incorrect (And potentially duplicable)
     -- barcode being uploaded. We never expect the cleaned barcode to be longer than 48 characters.
@@ -25,8 +16,8 @@ BEGIN
         RAISE EXCEPTION 'Please check barcode %s. Even after removing invalid characters, it is too long (Greater than 48 characters).', p_barcode;
     END IF;
 
-    -- Pad the barcode to 50 digits long.
-    v_clean_barcode := LPAD(v_clean_barcode, 50, '0');
+    -- Pad the barcode to 50 digits long and make it upper case for consistency.
+    v_clean_barcode := LPAD(UPPER(v_clean_barcode), 50, '0');
 
     -- Given a clean barcode, we now upsert the product.
     INSERT INTO dirac.product(name, description, barcode)
